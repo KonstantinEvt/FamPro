67a0d35259e53738757ff704545065bf7b019545
Konstantin Evtushenko <K-S-Y@yandex.ru>
2024-08-24 16:00:32 +0300

    Change #3

---
+++ src/main/java/com/family/fampro/controller/StorageController.java
@@ -0,0 +1,26 @@
+package com.family.fampro.controller;
+
+import com.family.fampro.service.ServiceOfStorageBD;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/Storage/")
+public class StorageController {
+    ServiceOfStorageBD serviceOfStorageBD;
+    @GetMapping("/save{filename}")
+    public ResponseEntity<String> saveDataInFile(@PathVariable String filename){
+        serviceOfStorageBD.saveDataToFile(filename);
+        return ResponseEntity.status(222).body("File is saved");
+    }
+    @GetMapping("/recover{filename}")
+    public  ResponseEntity<String> recoverBasefromFile(@PathVariable String filename){
+        serviceOfStorageBD.recoverBaseFromFile(filename);
+        return ResponseEntity.status(223).body("base is good");
+    }
+}
---
+++ src/main/java/com/family/fampro/service/ServiceOfStorageBD.java
@@ -0,0 +1,64 @@
+package com.family.fampro.service;
+
+import com.family.fampro.dto.FamilyMemberDto;
+import com.family.fampro.entity.FamilyMember;
+import com.family.fampro.mapper.FamilyMemberMapper;
+import com.family.fampro.repository.FamilyRepo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.*;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class ServiceOfStorageBD {
+    private final FamilyRepo familyRepo;
+    private final FamilyMemberMapper familyMemberMapper;
+    private final ServiceFM serviceFM;
+
+    public void saveDataToFile(String filename) {
+        try (FileWriter fr = new FileWriter("c:/FamPro/" + filename + ".txt")) {
+            Collection<FamilyMemberDto> familyMemberDtoList = serviceFM.getAllFamilyMembers();
+            ObjectMapper objectMapper = new ObjectMapper();
+            for (FamilyMemberDto fm : familyMemberDtoList) {
+                fr.write(objectMapper.writeValueAsString(fm));
+                fr.write('\n');
+            }
+        } catch (IOException e) {
+            log.warn("file is not saved:{?}", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void recoverBaseFromFile(String filename) {
+        Map<Long, FamilyMemberDto> map = new HashMap<>();
+        try (BufferedReader fr = new BufferedReader(new FileReader("c:/FamPro/" + filename + ".txt"))) {
+            String newFM = fr.readLine();
+            ObjectMapper ss = new ObjectMapper();
+            while (newFM != null) {
+                FamilyMemberDto familyMemberDto = ss.readValue(newFM, FamilyMemberDto.class);
+                map.put(familyMemberDto.getId(), familyMemberDto);
+                newFM = fr.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Collection<FamilyMember> listFM = familyMemberMapper.collectionDtoToCollectionEntity(map.values());
+        familyRepo.saveAll(listFM);
+        for (FamilyMemberDto fm : map.values()) {
+            FamilyMember familyMember = familyMemberMapper.dtoToEntity(fm);
+            if (fm.getFather_id() != null && map.containsKey(fm.getFather_id())) {
+                familyMember.setFather(familyMemberMapper.dtoToEntity(map.get(fm.getFather_id())));
+            }
+            if (fm.getMother_id() != null && map.containsKey(fm.getMother_id())) {
+                familyMember.setMother(familyMemberMapper.dtoToEntity(map.get(fm.getMother_id())));
+            }
+            familyRepo.save(familyMember);
+        }
+    }
+}
+
